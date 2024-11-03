65166e6a00bc0ded57a96e5dda06004567abd593
FahemAhmad <faheemahmad0108@gmail.com>
2024-11-03 13:53:30 +0500

    another cookie update

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -536,8 +536,11 @@ export const adminLogin = async (
       path: "/",
     };
 
-    if (process.env.NODE_ENV !== "production")
+    if (process.env.NODE_ENV === "production") {
+      cookieOptions.domain = ".medipals.co.uk";
+    } else {
       cookieOptions.domain = "localhost";
+    }
 
     // Set the session token as an HTTP-only cookie
     res.cookie("sessionToken", sessionToken, cookieOptions);
